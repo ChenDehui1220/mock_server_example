6c75ffbbd0ea66f7503f3f73a1e0f788419b5647
gary_chen <gary_chen@hiiir.com>
2017-12-04 16:24:31 +0800

    docs: try localhost from beta

--- code/mock/server_beta.js
+++ code/mock/server_beta.js
@@ -15,7 +15,7 @@ const filter = (pathname, req) => {
     return (routes[req.url] === undefined && !/(db|rules|\/get)/i.test(req.url))
 }
 
-const proxyUrl = 'http://gary-chen.rc.hiiir:4080/'
+const proxyUrl = 'http://localhost:8080'
 
 server.use(proxy(filter, { target: proxyUrl, changeOrigin: true }), (request, res, next) => {
     // request.method = 'GET'
