4475a25616baff87a9f886e4a4f56c240176af31
gary_chen <gary_chen@hiiir.com>
2017-12-04 15:47:11 +0800

    docs: fix server_xxx.js

--- code/mock/server_beta.js
+++ code/mock/server_beta.js
@@ -15,11 +15,7 @@ const filter = (pathname, req) => {
     return (routes[req.url] === undefined && !/(db|rules|\/get)/i.test(req.url))
 }
 
-let proxyUrl = 'http://localhost:8080'
-
-if (process.env.NODE_ENV !== undefined && process.env.NODE_ENV === 'beta') {
-    proxyUrl = 'http://gary-chen.rc.hiiir:4080/'
-}
+const proxyUrl = 'http://gary-chen.rc.hiiir:4080/'
 
 server.use(proxy(filter, { target: proxyUrl, changeOrigin: true }), (request, res, next) => {
     // request.method = 'GET'
